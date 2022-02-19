bfa21d6ec52140fc80cb634b4d4269bae37346e1
Samtray <sammychurro@gmail.com>
2022-02-18 18:01:28 -0800

    Proyecto Base
    Proyecto con las bases para ir desarrollando las mockups

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'landing', component:LandingPageComponent},
+  {path: 'profile', component:ProfileComponent},
+  {path: 'recipe', component:RecipeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ProfileComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
